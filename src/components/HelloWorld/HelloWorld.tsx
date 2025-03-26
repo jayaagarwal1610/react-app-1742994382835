16275f4f6d9429f00e2d1e4e5a2ea07491af1b98
Keshav Bansal <keshav.bansal@timesinternet.in>
2025-03-26 18:36:44 +0530

    Sync changes: modified: src/components/HelloWorld/HelloWorld.tsx

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HelloWorld: React.FC = () => {
   return (
     <div >
-      <h1 >Hello, World!</h1>
+      <h1 >Hello, World ok!</h1>
       <p >
         This is a basic React application created with Create React App and TypeScript.
       </p>
