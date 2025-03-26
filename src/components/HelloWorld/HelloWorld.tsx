dee5e30ba07bd4c0b8624c65e6f108407d68c51f
Keshav Bansal <keshav.bansal@timesinternet.in>
2025-03-26 18:36:27 +0530

    Initial commit

---
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div >
+      <h1 >Hello, World!</h1>
+      <p >
+        This is a basic React application created with Create React App and TypeScript.
+      </p>
+    </div>
+  );
+};
+
+export default HelloWorld;
